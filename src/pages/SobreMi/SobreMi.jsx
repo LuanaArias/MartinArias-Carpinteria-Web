67262c78f28291fff354e872828c79809bdbbd8a
Luana <luanaariaaas@gmail.com>
2026-02-21 18:45:34 -0300

    corriendo otro error para el deploy

--- src/pages/SobreMi/SobreMi.jsx
+++ src/pages/SobreMi/SobreMi.jsx
@@ -1,6 +1,6 @@
 import { SobreMiSeccionIcon } from "../../components/SobreMiSecciones/SobreMiSeccionIcon/SobreMiSeccionIcon";
 import { SobreMiSeccionPrinc } from "../../components/SobreMiSecciones/SobreMiSeccionPrinc/SobreMiSeccionPrinc";
-import { SobreMiSeccionTrabajo } from "../../components/SobreMiSecciones/SobreMiSeccionTrabajo/SobreMiSeccionTrabajo.jsx";
+import { SobreMiSeccionTrabajo } from "../../components/SobreMiSecciones/SobreMiSeccionTrabajo/SobreMiSeccionTrabajo";
 
 export function SobreMi(){
     return(
